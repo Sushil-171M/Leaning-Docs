434ed19b49a42695e7202b4d6487a35e386029fb
Sushil Maurya <104303944+Sushil-171M@users.noreply.github.com>
2024-08-27 17:46:40 +0530

    Updated React_Interview File
     added missing topics

--- React_Interview.js
+++ React_Interview.js
@@ -6,11 +6,10 @@ What is React ??
 It follows the Component based architecture.
 
 Why React ??
- 1) Component Based Architecture : We can break a big component into smaller component and make it reusable also, later we can merge all these component together for
-     the result.
+ 1) Component Based Architecture : We can break a big component into smaller component and make it reusable also, later we can merge all these component together for  the result.
  2) Use of Virtual DOM : 
                    DOM :  DOM stands for ‘Document Object Model’. 
-                   it is a tree structured representation of the HTML elements that are present in a webpage. DOM represents the entire UI of your application. 
+                   it is a tree structured representation of the HTML elements that are present in a webpage. DOM represents the entire UI 			of your application. 
                    The DOM is represented as a tree data structure.    
         Disadvantages of real DOM :
             Every time the DOM gets updated, the updated element and its children have to be rendered again to update the UI of our page. 
@@ -24,14 +23,16 @@ When code updated in the JavaScript file, these things happen:
     Recalculates the CSS for the parent and child nodes.
     Update the layout.
     Finally, traverse the tree and paint it on the screen(browser) display.
-    Recalculating the CSS and changing the layouts involves complex algorithms, and they do affect the performance
+    Recalculating the CSS and changing the layouts involves complex algorithms, and they do affect the performance.
 
 **************************************************************************************************************************************************************
-Note : Manipulating DOM is slow, but manipulating Virtual DOM is fast as nothing gets drawn on the screen.   
+Note : Manipulating Real DOM is slow, but manipulating Virtual DOM is fast as nothing gets drawn on the screen.   
 
 When anything new is added to the application, a virtual DOM is created and it is lightweight copy of Real-DOM.
 React maintains two Virtual DOM at each time, one contains the updated Virtual DOM and one which is just the pre-update version of this updated Virtual DOM.
- Now it compares the pre-update version with the updated Virtual DOM and figures out what exactly has changed in the DOM like which components have been changed. This process of comparing the current Virtual DOM tree with the previous one is known as ‘diffing’. Once React finds out what exactly has changed then it updates those objects only, on real DOM. 
+ Now it compares the pre-update version with the updated Virtual DOM and figures out what exactly has changed in the DOM like which components have been changed. This process of comparing the current Virtual DOM tree with the previous one is known as ‘diffing’. Once React finds out what exactly has changed then React-DOm updates those objects only, on real DOM. 
+
+Note:  It is React-DOm's task to update the real DOM not react.
 
 React uses something called batch updates to update the real DOM. It just means that the changes to the real DOM are sent in batches instead of sending any update for a single change in the state of a component. 
 
@@ -43,10 +44,10 @@ That is why React is much faster than other frameworks like anular or vue.
 Virtual DOM	                                                                                            Real DOM 
 It is a lightweight copy of the original DOM	                                                    It is a tree representation of HTML elements
 It is maintained by JavaScript libraries	                                                        It is maintained by the browser after parsing HTML elements
-After manipulation it only re-renders changed elements	                                            After manipulation, it re-render the entire DOM
+After manipulation it only re-renders changed elements	                                            After manipulation, it re-render the 															entire DOM
 Updates are lightweight	                                                                            Updates are heavyweight
-Performance is fast and UX is optimised	                                                             Performance is slow and the UX quality is low
-Highly efficient as it performs batch updates	                                                     Less efficient due to re-rendering of DOM after each update
+Performance is fast and UX is optimised	                                                             Performance is slow and the UX 															quality is low
+Highly efficient as it performs batch updates	                                                     Less efficient due to re-rendering of 														DOM after each update
 
 
 --------------------------------------------------------------------------**Limitation of React**----------------------------------------------
@@ -61,7 +62,7 @@ Highly efficient as it performs batch updates	                                  
 
 JSX : JSX stand for javascript XML. It is a templating langauge which allows us to write HTML code in inside JS file.
 
-1) In react , We can return a single JSx only,  if u want to return multiple then u have to combine in a single elemnent using div or <> tag.
+1) In react , We can return a single JSX only,  if u want to return multiple then u have to combine in a single elemnent using div or <> tag.
 2) JSX should
 
 
@@ -71,8 +72,8 @@ Diffing short for Differences Algorithm is used to differentiate the DOM Tree fo
 
 How Diffing Algorithm Works?
         First, the content is rendered on the webpage and the DOM tree is created.
-        On change in any content due to user interaction or change in data from API,React works on observable patterns, hence, whenever there is a change in the state, it updates the nodes in the virtual DOM
-        In reconcilliation the old tree is compared to the newest version to determine the number of changes needed for updation.
+        On change in any content due to user interaction or change in data from API,React works on observable patterns, hence, whenever 	there is a change in the state, it updates the nodes in the virtual DOM
+        In reconcilliation phase,  the old tree is compared to the newest version to determine the number of changes needed for updation.
         After determining the changes a set of optimized and minimal instruction is created to implement on the real DOM.
         These changes are then implemented and only content that changed is re-rendered on the web pages.
 
@@ -85,7 +86,7 @@ Advantages of Diffing Algorithm:
     It enhance the performance by updateing only the required components/ nodes.
     Results in faster respose while change by reducing the unwated and unnecessary re-renderings.
 
------------------------------------------------------------React Hooks ------------------------------
+-----------------------------------------------------------React Hooks ----------------------------------------------------------------------------
   React Hooks provide ability to use , states and other React features with the help of functional component wihtout writing class components.   
   Hooks are introduced in React 16.8.
 
@@ -93,8 +94,9 @@ Advantages of Diffing Algorithm:
     The Built-in React Hooks are:
 
     State Hooks :  used for state management.  Ex- useState,  useReducer
-    Context Hooks : Context hooks used to pass data from one component to another component without being passed as a prop.   ex - useContext
-    Ref Hooks : React Refs hooks creates the variable containing the information about DOM ex-> useRef (Declares a reference to the DOM elements mostly a DOM Node.)
+    Context Hooks : Context hooks used to pass data from one component to another component without being passed as a prop. Solved Prop Drilling issue.   ex - useContext
+    Ref Hooks : React Refs hooks creates the variable containing the information about DOM 
+           ex-> useRef (Declares a reference to the DOM elements mostly a DOM Node.)
     Effect Hooks : used to handle side Effects in react : ex-> useEffect
     Performance Hooks : Performace hooks are a way to skip the unnecessary work and optimise the rendering preformance.
             useMemo: return a memoized value reducing unnecessary computations (Ex-sort , filter).
@@ -103,9 +105,9 @@ Advantages of Diffing Algorithm:
     Benefits of using Hooks
         Hooks can improve code reusability and make it easier to split complex components into smaller functions.
 
-        Simpler, cleaner code: Functional components with hooks are often more concise and easier to understand than class components.
-        Better for complex UIs: Hooks make it easier to manage state and side effects in components.
-        Improved maintainability: Code using hooks is often easier to test and debug.
+        Simpler, cleaner code ::::: Functional components with hooks are often more concise and easier to understand than class components.
+        Better for complex UIs :::: Hooks make it easier to manage state and side effects in components.
+        Improved maintainability			 :: Code using hooks is often easier to test and debug.
 
 Why React Hooks ???????
   1) 'this' keyword :  u must have better understanding about this keyword. Implementing class components requires binding event handlers, adding complexity.
@@ -127,7 +129,7 @@ The three phases are: Mounting, Updating, and Unmounting.
 1. Initialization Phase : 
     In this phase, the developer has to define the props and initial state of the component this is generally done in the constructor of the class component.
 
-1) Mounting Phase :  Mounting is the phase of the component lifecycle when the initialization of the component is completed and the component is mounted on the DOM and rendered for the first time on the webpage.
+1) Mounting Phase :  Mounting is the first phase of the component lifecycle when the initialization of the component is completed and the component is mounted on the DOM and rendered for the first time on the webpage.
   ex->  
    constructor():
      Method to initialize state and bind methods. Executed before the component is mounted.
@@ -220,6 +222,11 @@ Reasons to Avoid Prop Drilling :
 
 3) useContext : The Context API in React provides a way to share values/data between components without having to pass props through every level of the tree. This helps to avoid prop drilling and simplifies state management across deeply nested components.
 
+Drawback of UseContext ::
+
+   **Performance Issues: Updating the context value can potentially cause unnecessary re-renders in consuming components, even if the changes are not relevant to them.
+   ** Scaling Challenges: The Context API can be hard to scale and maintain for a large-scale app. You have to manage the dependencies and updates of multiple contexts, which can lead to code duplication and inconsistency.
+   **Lack of Type Safety: Context values are not type-checked by default, which means that incorrect usage or changes in the shape of the context value may not be caught by the compiler or development tools. This can lead to runtime errors and debugging challenges.
 
 4) useReducer :  A useReducer is a hook in React that allows you add a reducer to your component. 
                 It takes in the reducer function and an initialState as arguments. 
@@ -456,7 +463,8 @@ Lifting state up is a technique where the state is moved to a common ancestor co
 
 controlled vs uncontrolled component :
 
-In a controlled component, form data is handled by a React component. The alternative is uncontrolled components, where form data is handled by the DOM itself.
+In a controlled component, form data is handled by a React component. 
+The alternative is uncontrolled components, where form data is handled by the DOM itself.
 
 
 stateless vs statefull component :
@@ -556,5 +564,119 @@ What is state scheduling ?
 What is state batching . please explain with example
  
 Batching, introduced in React 18, allows React to group multiple state updates that occur within a single task or event handler into a single batch. This means that React won't re-render the component after each individual state update but will instead wait until the end of the batch to perform the re-render.
+
+Higher Order COmponent ??
+  A higher-order component is a function that takes a component and returns a new component with enhanced functionality. It's essentially a function that wraps around a component to provide additional features or behavior.
+
+Why HOCs ??
+ - to share common functionality between components then we can create a HOC and share. similiar like prop Drilling.
+ Suppose there is nested component. and many component using same function , then we have pass through props to avoid that we can use HOCs.
+
+Bundling ??
+ Bundling is the process of following imported files and merging them into a single file: a “bundle”. This bundle can then be included on a webpage to load an entire app at once.
+
+Code Splitting :
+It allows you to split your JavaScript bundle into smaller chunks. With code splitting, you can only load the code that is needed in a particular context, reducing the initial load time and improving the overall user experience. 
+  Bundling is great, but as your app grows, your bundle will grow too. Especially if you are including large third-party libraries.
+ A large bundle takes longer to load, especially on slower networks or mobile devices, leading to increased load times.
+  To avoid winding up with a large bundle, it’s good to get ahead of the problem and start “splitting” your bundle. 
+
+Code-Splitting is a feature supported by bundlers like Webpack, Rollup and Browserify (via factor-bundle) which can create multiple bundles that can be dynamically loaded at runtime.
  
+For more Details-> ( https://medium.com/@shriharim006/code-splitting-in-react-optimize-performance-by-splitting-your-code-e3e70d0c3d91 )
+                   https://www.youtube.com/watch?v=6CYCD7kE8Qs
+
+
+ Render Props :  The Render Props is a technique in ReactJS for sharing code between React components using a prop whose value is a function.
+ 
+ defer vs async :: With async, the script executes as soon as it's downloaded, potentially before the HTML document is fully parsed. With defer, 
+ the script executes only after the HTML document is fully parsed, but before the DOMContentLoaded event.
+
+
+ -----------------------------------Data Binding-----------------------------------------
+
+ One-way data binding :: 
+One-way means that the binding happens in one direction. In this case, changes in the data automatically update the UI, but changes in the UI do not automatically update the data. That’s why it is referred to as one-way data binding.
+
+React achieves one-way data binding by using state and props.
+Ex-> Increment button, click on the button , it is incrementing the state value and it is reflecting in the UI also.
+
+
+Two-way data binding ::
+Two-way data binding allows bidirectional data flow, meaning that changes in the UI automatically update the component’s state, and changes in the state automatically update the UI. In React, two-way data binding is achieved using controlled components.
+Controlled components are form elements whose values are controlled by the state. They maintain a consistent, bidirectional data flow between the UI components and the data models.
+
+
+*********************************      Debouncing vs Throttling   **********************************
+Throttling is useful in scenarios like scroll event handlers, resize event handlers, or handling user input events like mousemove or keydown, where frequent calls can lead to performance degradation.
+
+Debouncing is typically used in scenarios where a function is called repeatedly, such as in response to user input like typing.
+
+
+********************************* ProtoType ***********************************
+
+every function in JavaScript has a prototype property that references an object.
+it allows us to share methods across all the instances of a function. 
+
+function Animal (name, energy) {
+  // const this = Object.create(Animal.prototype)
+
+  this.name = name
+  this.energy = energy
+
+  // return this
+}
+
+const leo = new Animal('Leo', 7)
+const snoop = new Animal('Snoop', 10)
+
+
+
+Note:: when we r calling constructor with new keyword, it will create a this keyword for us then
+commneted line we don't need to write (object creation and return part).
+but if u r calling like this
+const leo =  Animal('Leo', 7)
+
+then u have to use those line otherwise it will not work.
+
+By default, the prototype object will have a constructor property which points to the original function or the class that the instance was created from
+
+
+*********************************** Validation in React **************************************************
+
+1. Using PropTypes for Type Checking
+    PropTypes allow you to validate the type and presence of props passed to your components.
+    This is useful for ensuring that the data your components receive is of the expected type.
+
+    Ex-> 
+    MyComponent.propTypes = {
+            name: PropTypes.string.isRequired,  // name must be a string and required
+            age: PropTypes.number.isRequired,   // age must be a number and required
+            isMember: PropTypes.bool            // isMember must be a boolean
+};
+
+2. Using Controlled Components and State
+      For form validation, you can use controlled components where the form data is handled by the React state. 
+      You can validate the form data as the user types or when they submit the form.(manually validating like length of password, 
+      value is there or not , etc.  using if else cases)
+
+3.  Custom validation  ::  allows you to implement business-specific logic (based on requirement like u have implemented for LDAP URL)  
+
+4. Using the library like formik or react-hook-form
+
 `
+
+Reference :
+
+HOC :  https://medium.com/@vitorbritto/react-design-patterns-compound-component-pattern-ec247f491294
+Code Splitting : https://medium.com/@shriharim006/code-splitting-in-react-optimize-performance-by-splitting-your-code-e3e70d0c3d91
+OTP Generator : https://medium.com/@tejas.shirnalkar/building-otp-and-password-inputs-with-validation-in-react-step-by-step-fda703742088
+React Interview : https://github.com/sudheerj/reactjs-interview-questions?tab=readme-ov-file#when-to-use-a-class-component-over-a-function-component
+Controlled vs uncontrolled : https://www.freecodecamp.org/news/what-are-controlled-and-uncontrolled-components-in-react/
+useReducer : https://www.freecodecamp.org/news/react-usereducer-hook/
+Data Binding : https://handsontable.com/blog/understanding-data-binding-in-react
+
+React feaure must know : https://betterprogramming.pub/these-are-the-concepts-you-should-know-in-react-js-after-you-learn-the-basics-2023-edition-9594f3d0f6d3
+throttling :: https://www.freecodecamp.org/news/throttling-in-javascript/
+
+React Best Practices : https://www.tatvasoft.com/blog/reactjs-best-practices/
